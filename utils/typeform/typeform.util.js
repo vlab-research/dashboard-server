b0eefc14de935737fb27476055992cebb9c90440
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2019-06-19 18:05:16 +0200

    feat: user Oauth with typeform

--- utils/typeform/typeform.util.js
+++ utils/typeform/typeform.util.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const request = require('request');
-const { TYPEFORM: { typeformUrl, clientId, secret, redirectUri } } = require('../../config');
+const {
+  TYPEFORM: { typeformUrl, clientId, secret, redirectUri },
+} = require('../../config');
 
 function TypeformToken(code) {
-  return new Promise((resolve, reject) =>{
-   const options = {
+  return new Promise((resolve, reject) => {
+    const options = {
       url: `${typeformUrl}/oauth/token`,
       form: {
         grant_type: 'authorization_code',
@@ -13,47 +15,47 @@ function TypeformToken(code) {
         client_id: clientId,
         client_secret: secret,
         redirect_uri: redirectUri,
-      }
-    }
+      },
+    };
     request.post(options, (err, _, body) => {
-      if (err) reject(err)
+      if (err) reject(err);
       resolve(JSON.parse(body));
-    })
-  })
+    });
+  });
 }
 
 function TypeformFormList(token) {
-  return new Promise((resolve, reject) =>{
-   const options = {
+  return new Promise((resolve, reject) => {
+    const options = {
       url: `${typeformUrl}/forms`,
       headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }
+        Authorization: `Bearer ${token}`,
+      },
+    };
     request.get(options, (err, _, body) => {
-      if (err) reject(err)
+      if (err) reject(err);
       resolve(JSON.parse(body));
-    })
-  })
+    });
+  });
 }
 
 function TypeformForm(token, formId) {
-  return new Promise((resolve, reject) =>{
-   const options = {
+  return new Promise((resolve, reject) => {
+    const options = {
       url: `${typeformUrl}/forms/${formId}`,
       headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    }
+        Authorization: `Bearer ${token}`,
+      },
+    };
     request.get(options, (err, _, body) => {
-      if (err) reject(err)
+      if (err) reject(err);
       resolve(body);
-    })
-  })
+    });
+  });
 }
 
 module.exports = {
   TypeformToken,
   TypeformFormList,
-  TypeformForm
+  TypeformForm,
 };
