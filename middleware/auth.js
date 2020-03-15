9b7aa9a3b93a358171500605ac2ae6e4a0faead9
Nandan <nandanmarkrao@gmail.com>
2020-03-15 18:52:41 +0100

    notes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,8 @@ const { JWT: clientConfig, SERVER_JWT: serverConfig } = require('../config');
 
 // make middleware that tries auth0 client then if that fails
 // tries auth0 server application...
+
+// TODO: change the latter to a basic auth strategy for server-to-server
 function auth(req, res, next) {
   jwt(clientConfig)(req, res, (err, res) => {
     if (!err) return next();
